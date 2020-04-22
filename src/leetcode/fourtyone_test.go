b66513605e3a40b14efd2b1b8f7f81ef6eaf9082
wodiwudi <2039725039@qq.com>
2020-04-22 14:25:35 +0800

    test

--- src/leetcode/fourtyone_test.go
+++ src/leetcode/fourtyone_test.go
@@ -1,1 +1,26 @@
 package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func exchange(nums []int) []int {
+	//快慢指针解法
+	i, j := 0, 0
+	length := len(nums)
+	for j < length {
+		if nums[j]&1 != 0 {
+			//奇数
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+		}
+		j++
+	}
+	return nums
+}
+
+func TestFourtyOne(t *testing.T) {
+	nums := []int{1, 2, 3, 4}
+	fmt.Println(exchange(nums))
+}
